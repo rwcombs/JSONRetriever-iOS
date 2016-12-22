d414bb70188b37e07b9dea610e40c64df858221e
Wesley Combs <wesley.combs@gmail.com>
2016-12-22 15:49:54 -0500

    Added http requests header options to the code

--- JSON Retriever/ViewController.swift
+++ JSON Retriever/ViewController.swift
@@ -1,56 +1,88 @@
-//
-//  ViewController.swift
-//  JSON Retriever
-//
-//  Created by Wesley Combs on 12/22/16.
-//  Copyright © 2016 Wesley Combs. All rights reserved.
-//
-//  Built using the tutorial by Sebastian Henry aka The Swift Guy
-//  https://www.youtube.com/watch?v=-ddIP7NRkmY
-
 import UIKit
 
+
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad()
-    {
+    
+    
+    
+    override func viewDidLoad() {
+        
         super.viewDidLoad()
-    
-        // Getting Data
-        let url = URL(string:"http://api.fixer.io/latest")
-        //let url = URL(string: "http://mirthfhir.ddns.net/dstu2/Patient/10")
-        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            
-            if error != nil
-            {
-                print("ERROR")
-            }
-            else
-            {
-                if let content = data
-                {
-                    do
-                    {
-                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                        
-                        print (myJson)
-                    }
-                    catch
-                    {
-                    
-                    }
-                }
-            }
-        }
-        task.resume()
-    
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        runStuff()
+        
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
+    
+    
+    
+    func runStuff()
+        
+    {
+        
+        
+        
+        let string = "http://mirthfhir.ddns.net/dstu2/patient"
+        //let string = "http://api.fixer.io/latest" // Dont include any HTTP headers
+        let url = NSURL(string: string)
+        let request = NSMutableURLRequest(url: url! as URL)
+        request.httpMethod = "GET"
+        
+        //request.addValue("application/json+fhir", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        
+        
+        
+        let myTask = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
+            
+            
+            
+            if let content = data
+                
+            {
+                
+                do
+                    
+                {
+                    
+                    let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                    
+                    
+                    print(myJson)
+                    
+                }
+                    
+                catch
+                    
+                {
+                    
+                    
+                    
+                }
+                
+            }
+            
+        }
+        
+        myTask.resume()
+        
+    }
+    
 }
 
